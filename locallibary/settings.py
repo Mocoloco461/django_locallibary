cee36c7272f71469b92a9e19170700856f99e8f5
Mocoloco461 <nuhnuh0101@gmail.com>
2025-11-12 10:17:16 +0200

    :|

--- locallibary/settings.py
+++ locallibary/settings.py
@@ -40,14 +40,15 @@ SECRET_KEY = os.environ.get(
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
-ALLOWED_HOSTS = ['djangolocallibary-production.up.railway.app', '127.0.0.1']
-ALLOWED_HOSTS = ['*', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'djangolocallibary-production.up.railway.app',
+    '127.0.0.1',
+    'localhost',
+]
 
-
-# 
-#CSRF_TRUSTED_ORIGINS = ['https://djangolocallibary-production.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ['https://*','http://*']
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://djangolocallibary-production.up.railway.app',
+]
 
 
 # Application definition
