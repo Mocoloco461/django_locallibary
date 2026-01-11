22f4e451794e83b34b5543196fbf907bfc873f96
Mocoloco461 <nuhnuh0101@gmail.com>
2026-01-11 13:41:08 +0200

    we are luch

--- terraform/scripts/set_first_master.sh
+++ terraform/scripts/set_first_master.sh
@@ -3,4 +3,4 @@ apt install -y
 apt install curl -y
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --token ${AGENT_TOKEN}" sh -
 
-git clone -b conf-k3s-files git@gitlab.com:Mocoloco461/django_locallibary.git
+git clone -b conf-k3s-files https://gitlab.com/Mocoloco461/django_locallibary.git
