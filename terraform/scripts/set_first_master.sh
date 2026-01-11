e2d507fa3b57480ca29b405e4aa5c30b6517161b
Mocoloco461 <nuhnuh0101@gmail.com>
2026-01-11 12:58:11 +0200

    fix image tag & git clone auto

--- terraform/scripts/set_first_master.sh
+++ terraform/scripts/set_first_master.sh
@@ -2,4 +2,5 @@
 apt install -y
 apt install curl -y
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --token ${AGENT_TOKEN}" sh -
- 
+
+git clone -b conf-k3s-files git@gitlab.com:Mocoloco461/django_locallibary.git
