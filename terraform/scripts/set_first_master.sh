d18907a578cc161b00910221bb36df36496c9398
Mocoloco461 <nuhnuh0101@gmail.com>
2026-01-26 11:25:40 +0200

    helms files reday

--- terraform/scripts/set_first_master.sh
+++ terraform/scripts/set_first_master.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-apt install -y
+# apt install -y
 apt install curl -y
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --token ${AGENT_TOKEN}" sh -
 sleep 15
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-git clone -b prodtest https://gitlab.com/Mocoloco461/django_locallibary.git
-kubectl apply -f /django_locallibary/k3s
+
+
+# git clone -b prodtest https://gitlab.com/Mocoloco461/django_locallibary.git
+# kubectl apply -f /django_locallibary/k3s
 
 
