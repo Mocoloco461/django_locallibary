3d02a6f189cbf987878d8d320ed03af912f25479
Mocoloco461 <nuhnuh0101@gmail.com>
2026-01-29 13:11:01 +0200

    modify files and remove helm to diffrent repo

--- helm/talktome.md
+++
@@ -1,6 +0,0 @@
-https://gemini.google.com/app/d1a8a26ac68fd33f?utm_source=gemini&utm_medium=web&utm_campaign=gemini_veo2_lp_deeplink&hl=en
-
-
-# דרך הקישור הזה תמשיך את התהליך שניסית להתחיל לעשות
-
-a
--- terraform/scripts/set_agents.sh
+++ terraform/scripts/set_agents.sh
@@ -3,5 +3,11 @@ apt install
 apt install curl -y
 curl -sfL https://get.k3s.io | K3S_URL=https://${MASTER_A_IP}:6443 K3S_TOKEN=${AGENT_TOKEN} sh -
 
+# set up argo cd:
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+
+
 # set here a agent - need a vpc to get a static ip etc.
 #server --cluster-init --token ${AGENT_TOKEN} --node-name k3s-master 
