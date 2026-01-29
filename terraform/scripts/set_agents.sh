c97a5ca0f25ef5a367f50a892787999c1ed7d575
Mocoloco461 <nuhnuh0101@gmail.com>
2026-01-29 14:30:09 +0200

    fix scripts

--- terraform/scripts/set_agents.sh
+++ terraform/scripts/set_agents.sh
@@ -3,9 +3,7 @@ apt install
 apt install curl -y
 curl -sfL https://get.k3s.io | K3S_URL=https://${MASTER_A_IP}:6443 K3S_TOKEN=${AGENT_TOKEN} sh -
 
-# set up argo cd:
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
 
 
 
--- terraform/scripts/set_first_master.sh
+++ terraform/scripts/set_first_master.sh
@@ -5,6 +5,20 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --token ${AGENT_TOKEN}" 
 sleep 15
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
+until kubectl get nodes; do
+  echo "Waiting for K3s API server..."
+  sleep 5
+done
+
+# set up argo cd:
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+# pull the app config repo:
+git clone https://github.com/Mocoloco461/locallibary-configs
+sleep 5
+kubectl apply -f locallibary-configs/argocd.yml
+
 
 # git clone -b prodtest https://gitlab.com/Mocoloco461/django_locallibary.git
 # kubectl apply -f /django_locallibary/k3s
