4b7a7413ab06a817c542a0badad7ce28156c47d8
Mocoloco461 <nuhnuh0101@gmail.com>
2026-02-05 14:01:36 +0200

    fix ingress

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -33,9 +33,23 @@ resource "aws_security_group" "open_to_the_world" {
   vpc_id      = aws_vpc.locallibary.id
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
